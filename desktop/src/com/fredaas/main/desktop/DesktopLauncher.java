cb6782a1e6cb032b0860ff73268042ac9f60e566
fredaas <fredaas86@gmail.com>
2016-02-26 19:38:03 +0100

    Update desktop launcher.

--- desktop/src/com/fredaas/main/desktop/DesktopLauncher.java
+++ desktop/src/com/fredaas/main/desktop/DesktopLauncher.java
@@ -1,7 +1,5 @@
 package com.fredaas.main.desktop;
 
-import java.awt.Toolkit;
-
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.fredaas.main.Game;
@@ -9,14 +7,8 @@ import com.fredaas.main.Game;
 public class DesktopLauncher {
     
 	public static void main (String[] arg) {
-	    int DEVICE_WIDTH = Toolkit.getDefaultToolkit().getScreenSize().width;
-	    int DEVICE_HEIGHT = Toolkit.getDefaultToolkit().getScreenSize().height;
-		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-		cfg.width = 800;
-		cfg.height = 600;
-		cfg.resizable = false;
-		cfg.fullscreen = false;
-		new LwjglApplication(new Game(), cfg);
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
 	}
 	
 }
